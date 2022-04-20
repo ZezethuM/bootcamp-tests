fe6d99f24b5b1b1aa29e74cbd1c03cf6e113c819
ZezethuM <zezethu15@gmail.com>
2022-04-20 14:29:33 +0200

    modified tests files

--- test/findItemsOver20.tests.js
+++ test/findItemsOver20.tests.js
@@ -1,10 +1,10 @@
 describe('Test function findItemsOver20', function(){
     it('it should return a list of all the items that have quantity over 20', function(){
-    assert.deepEqual([{name: 'pears', qty: 37}, {name: 'bananas', qty: 27}], findItemsOver20(itemList));
-    assert.deepEqual([{name: 'pears', qty: 37}, {name: 'bananas', qty: 27}], findItemsOver20(results));
-    assert.deepEqual([], findItemsOver20(results2));
+    assert.deepEqual(results, findItemsOver20(itemList));
+    assert.deepEqual(results, findItemsOver20(results));
+    assert.deepEqual(results2, findItemsOver20(results2));
 });
     it('it should return an empty array when another data type is inputed', function(){
-        assert.deepEqual([], findItemsOver20(''));
+        assert.deepEqual(results2, findItemsOver20(''));
     });
 });
