8b299e332d9a0107fa9d66ccc1a0ff1f6298c58a
ZezethuM <zezethu15@gmail.com>
2022-05-02 22:29:46 +0200

    added data

--- test/findItemsOver20.tests.js
+++ test/findItemsOver20.tests.js
@@ -1,3 +1,37 @@
+var itemList = [
+    {name : 'apples', qty : 10},
+    {name : 'pears', qty : 37},
+    {name : 'bananas', qty : 27},
+    {name : 'apples', qty : 3},
+  ];
+  
+  var results = [
+    {name : 'pears', qty : 37},
+    {name : 'bananas', qty : 27},
+  ];
+  
+  var itemList2 = [
+    {name : 'apples', qty : 10},
+    {name : 'pears', qty : 19},
+    {name : 'bananas', qty : 17},
+    {name : 'apples', qty : 3},
+  ];
+  
+  var results2 = [];
+  
+  var itemList3 = [
+    {name : 'apples', qty : 40},
+    {name : 'pears', qty : 20},
+    {name : 'bananas', qty : 23},
+    {name : 'apples', qty : 37}
+  ];
+  
+  var results3 = [
+    {name : 'apples', qty : 40},
+    {name : 'bananas', qty : 23},
+    {name : 'apples', qty : 37}
+  ];
+
 describe('Test function findItemsOver20', function(){
     it('it should return a list of all the items that have quantity over 20', function(){
     assert.deepEqual(results, findItemsOver20(itemList));
